8b22a7b4c0e165dc5fe7ef2c192b76b97a06c334
Arslyyy <irfoputrablora@gmail.com>
2026-06-10 13:56:04 +0700

    tambah fitur crud

--- mahasiswa/urls.py
+++ mahasiswa/urls.py
@@ -6,4 +6,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('', RedirectView.as_view(url='daftar/')),
     path('daftar/', views.daftar_mahasiswa, name='daftar_mahasiswa'),
+    path('tambah/', views.tambah_mahasiswa, name='tambah_mahasiswa'),
+    path('edit/<int:pk>/', views.edit_mahasiswa, name='edit_mahasiswa'),
+    path('hapus/<int:pk>/', views.hapus_mahasiswa, name='hapus_mahasiswa'),
 ]
--- mahasiswa/views.py
+++ mahasiswa/views.py
@@ -1,7 +1,7 @@
 from urllib import request
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Mahasiswa
 
@@ -24,3 +24,66 @@ def index_template(request):
 def daftar_mahasiswa(request):
     mahasiswas = Mahasiswa.objects.all()
     return render(request, 'mahasiswa/daftar.html', {'mahasiswas': mahasiswas})
+
+@login_required(login_url='/accounts/login/')
+def tambah_mahasiswa(request):
+    """Create new mahasiswa"""
+    if request.method == 'POST':
+        nim = request.POST.get('nim')
+        nama = request.POST.get('nama')
+        programstudi = request.POST.get('programstudi')
+        angkatan = request.POST.get('angkatan')
+        
+        # Validasi
+        if nim and nama and programstudi and angkatan:
+            # Check if nim already exists
+            if Mahasiswa.objects.filter(nim=nim).exists():
+                error = 'NIM sudah terdaftar!'
+                return render(request, 'mahasiswa/form_mahasiswa.html', {'error': error})
+            
+            # Create new mahasiswa
+            mahasiswa = Mahasiswa(
+                nim=nim,
+                nama=nama,
+                programstudi=programstudi,
+                angkatan=int(angkatan)
+            )
+            mahasiswa.save()
+            return redirect('daftar_mahasiswa')
+        else:
+            error = 'Semua field harus diisi!'
+            return render(request, 'mahasiswa/form_mahasiswa.html', {'error': error})
+    
+    return render(request, 'mahasiswa/form_mahasiswa.html')
+
+@login_required(login_url='/accounts/login/')
+def edit_mahasiswa(request, pk):
+    """Update mahasiswa"""
+    mahasiswa = get_object_or_404(Mahasiswa, pk=pk)
+    
+    if request.method == 'POST':
+        mahasiswa.nim = request.POST.get('nim')
+        mahasiswa.nama = request.POST.get('nama')
+        mahasiswa.programstudi = request.POST.get('programstudi')
+        mahasiswa.angkatan = int(request.POST.get('angkatan'))
+        
+        # Validasi
+        if mahasiswa.nim and mahasiswa.nama and mahasiswa.programstudi and mahasiswa.angkatan:
+            mahasiswa.save()
+            return redirect('daftar_mahasiswa')
+        else:
+            error = 'Semua field harus diisi!'
+            return render(request, 'mahasiswa/form_mahasiswa.html', {'error': error, 'mahasiswa': mahasiswa})
+    
+    return render(request, 'mahasiswa/form_mahasiswa.html', {'mahasiswa': mahasiswa})
+
+@login_required(login_url='/accounts/login/')
+def hapus_mahasiswa(request, pk):
+    """Delete mahasiswa"""
+    mahasiswa = get_object_or_404(Mahasiswa, pk=pk)
+    
+    if request.method == 'POST':
+        mahasiswa.delete()
+        return redirect('daftar_mahasiswa')
+    
+    return render(request, 'mahasiswa/konfirmasi_hapus.html', {'mahasiswa': mahasiswa})
